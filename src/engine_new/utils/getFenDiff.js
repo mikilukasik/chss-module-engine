c6be75ef4f6cdd669f8579f13c43f221092c744d
mikilukasik <szarhazi@gmail.com>
2023-03-19 00:00:48 +0000

    .

--- src/engine_new/utils/getFenDiff.js
+++ src/engine_new/utils/getFenDiff.js
@@ -1,59 +1,159 @@
-export const getFenDiff = (prevFen, currFen) => {
-  const prevRows = prevFen.split(" ")[0].split("/");
-  const currRows = currFen.split(" ")[0].split("/");
+const parseFen = (fen) => {
+  const positions = fen.split(" ")[0].split("/");
+  const board = [];
 
-  const addedPieces = [];
-  const removedPieces = [];
-  const movedPieces = [];
+  positions.forEach((row, rowIndex) => {
+    board[rowIndex] = [];
+    let columnIndex = 0;
 
-  for (let i = 0; i < prevRows.length; i++) {
-    const prevRow = prevRows[i];
-    const currRow = currRows[i];
-
-    let prevCol = 0;
-    let currCol = 0;
-
-    while (prevCol < prevRow.length || currCol < currRow.length) {
-      const prevChar = prevRow.charAt(prevCol);
-      const currChar = currRow.charAt(currCol);
-
-      if (prevChar === currChar) {
-        prevCol++;
-        currCol++;
-        continue;
+    for (const char of row) {
+      if (isNaN(parseInt(char))) {
+        board[rowIndex][columnIndex] = char;
+        columnIndex++;
+      } else {
+        for (let i = 0; i < parseInt(char); i++) {
+          board[rowIndex][columnIndex] = "-";
+          columnIndex++;
+        }
       }
+    }
+  });
 
-      if (currChar === undefined) {
-        // Piece has been removed
-        removedPieces.push(`${String.fromCharCode(97 + prevCol)}${8 - i}`);
-        prevCol++;
-        continue;
+  return board;
+};
+
+const findDifferences = (prevBoard, currBoard) => {
+  const addedPieces = {};
+  const removedPieces = {};
+
+  for (let i = 0; i < prevBoard.length; i++) {
+    for (let j = 0; j < prevBoard[i].length; j++) {
+      const prevPiece = prevBoard[i][j];
+      const currPiece = currBoard[i][j];
+
+      if (prevPiece !== currPiece) {
+        if (prevPiece !== "-") {
+          removedPieces[prevPiece] = [
+            ...(removedPieces[prevPiece] || []),
+            [j, i],
+          ];
+        }
+
+        if (currPiece !== "-") {
+          addedPieces[currPiece] = [...(addedPieces[currPiece] || []), [j, i]];
+        }
       }
-
-      if (prevChar === undefined) {
-        // Piece has been added
-        addedPieces.push({
-          piece: currChar.toLowerCase(),
-          square: `${String.fromCharCode(97 + currCol)}${8 - i}`,
-        });
-        currCol++;
-        continue;
-      }
-
-      // Piece has been moved
-      movedPieces.push({
-        piece: currChar.toLowerCase(),
-        from: `${String.fromCharCode(97 + prevCol)}${8 - i}`,
-        to: `${String.fromCharCode(97 + currCol)}${8 - i}`,
-      });
-      prevCol++;
-      currCol++;
     }
   }
 
   return {
     addedPieces,
     removedPieces,
+  };
+};
+
+const getDistance = (source, target) =>
+  Math.abs(source[0] - target[0]) + Math.abs(source[1] - target[1]);
+
+const getMoveOptionsByDistance = (sourceSquares, targetSquares) => {
+  const moveOptions = sourceSquares
+    .map((source) =>
+      targetSquares.map((target) => {
+        const distance = getDistance(source, target);
+
+        return {
+          distance,
+          source: `${String.fromCharCode(97 + source[0])}${8 - source[1]}`,
+          target: `${String.fromCharCode(97 + target[0])}${8 - target[1]}`,
+        };
+      })
+    )
+    .flat();
+
+  return moveOptions.sort((a, b) => b.distance - a.distance);
+};
+
+export const getFenDiff = (prevFen, currFen) => {
+  const prevBoard = parseFen(prevFen);
+  const currBoard = parseFen(currFen);
+
+  const { addedPieces, removedPieces } = findDifferences(prevBoard, currBoard);
+
+  const movedPieces = [];
+  const addedPiecesResult = [];
+  const removedPiecesResult = [];
+
+  for (const piece of Object.keys({ ...removedPieces, ...addedPieces })) {
+    const sourceSquares = removedPieces[piece];
+    const targetSquares = addedPieces[piece];
+    const targetSquaresAsStrings = (targetSquares || []).map(
+      ([j, i]) => `${String.fromCharCode(97 + j)}${8 - i}`
+    );
+
+    if (!sourceSquares || !sourceSquares.length) {
+      addedPiecesResult.push(
+        ...targetSquaresAsStrings.map((square) => ({ square, piece }))
+      );
+      continue;
+    }
+
+    const sourceSquaresAsStrings = sourceSquares.map(
+      ([j, i]) => `${String.fromCharCode(97 + j)}${8 - i}`
+    );
+
+    if (!targetSquares || !targetSquares.length) {
+      removedPiecesResult.push(
+        ...sourceSquaresAsStrings.map((square) => ({ square }))
+      );
+      continue;
+    }
+
+    const moveOptionsByDistance = getMoveOptionsByDistance(
+      sourceSquares,
+      targetSquares
+    );
+
+    while (
+      sourceSquaresAsStrings.length &&
+      targetSquaresAsStrings.length &&
+      moveOptionsByDistance.length
+    ) {
+      const possibleMove = moveOptionsByDistance.pop();
+      if (
+        sourceSquaresAsStrings.includes(possibleMove.source) &&
+        targetSquaresAsStrings.includes(possibleMove.target)
+      ) {
+        movedPieces.push({
+          piece,
+          from: possibleMove.source,
+          to: possibleMove.target,
+        });
+
+        sourceSquaresAsStrings.splice(
+          sourceSquaresAsStrings.indexOf(possibleMove.source),
+          1
+        );
+        targetSquaresAsStrings.splice(
+          targetSquaresAsStrings.indexOf(possibleMove.target),
+          1
+        );
+      }
+    }
+
+    addedPiecesResult.push(
+      ...targetSquaresAsStrings.map((square) => ({
+        square,
+        piece,
+      }))
+    );
+    removedPiecesResult.push(
+      ...sourceSquaresAsStrings.map((square) => ({ square }))
+    );
+  }
+
+  return {
+    addedPieces: addedPiecesResult,
+    removedPieces: removedPiecesResult,
     movedPieces,
   };
 };
